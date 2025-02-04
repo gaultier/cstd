b26ae176f59cc4d9f79897dd40b46461711e347d
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-04 13:12:08 +0100

    trim

--- lib.c
+++ lib.c
@@ -5609,9 +5609,6 @@ static void pg_task_runner_handle_task_sqes(PgTaskRunner *runner,
     return;
   }
 
-  // Need to map a cqe to: sqe->kind, Task.
-  // => Can just store Task* in user_data and use the unused bits in the
-  // pointer to store sqe->kind.
   PgIoSubmissionEvent sqe = {0};
   while (pg_ring_read_struct(&task->outbox_task_runner, &sqe)) {
     switch (sqe.kind) {
