f03fae7e2e8012736981dc358c8ab41deba9b852
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-02-12 20:28:56 +0100

    add err

--- lib.c
+++ lib.c
@@ -91,12 +91,12 @@ typedef u32 PgError;
 #include <errno.h>
 #define PG_ERR_INVALID_VALUE EINVAL
 #define PG_ERR_IO EIO
+#define PG_ERR_TOO_BIG E2BIG
 #else
 // Use the x86_64 Linux errno values.
 #define PG_ERR_INVALID_VALUE 22
 #define PG_ERR_IO 5
-#define PG_ERR_AGAIN 11
-#define PG_ERR_IN_PROGRESS 115
+#define PG_ERR_TOO_BIG 7
 #endif
 
 PG_RESULT(u64) PgU64Result;
