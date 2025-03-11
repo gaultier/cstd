da61914fad3c3279fe305fa613dde7805095cacc
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-11 09:13:00 +0100

    test

--- test.c
+++ test.c
@@ -1994,8 +1994,8 @@ static void test_html_tokenize_nested() {
   {
     PgHtmlToken token = PG_SLICE_AT(tokens, 2);
     PG_ASSERT(PG_HTML_TOKEN_KIND_TEXT == token.kind);
-    /* PG_ASSERT(29 == token.start); */
-    /* PG_ASSERT(35 == token.end); */
+    PG_ASSERT(16 == token.start);
+    PG_ASSERT(21 == token.end);
     PG_ASSERT(pg_string_eq(PG_S("foo"), pg_string_trim_space(token.text)));
   }
 
