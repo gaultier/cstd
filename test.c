0f89ca9a028c5f072416f2f829dd4be0e04c62e2
Philippe Gaultier <philippe.gaultier@gi-de.com>
2025-03-11 09:19:47 +0100

    test

--- test.c
+++ test.c
@@ -2002,8 +2002,8 @@ static void test_html_tokenize_with_key_no_value() {
   {
     PgHtmlToken token = PG_SLICE_AT(tokens, 3);
     PG_ASSERT(PG_HTML_TOKEN_KIND_TAG_CLOSING == token.kind);
-    /* PG_ASSERT(37 == token.start); */
-    /* PG_ASSERT(41 == token.end); */
+    PG_ASSERT(18 == token.start);
+    PG_ASSERT(22 == token.end);
     PG_ASSERT(pg_string_eq(PG_S("html"), token.tag));
   }
 }
