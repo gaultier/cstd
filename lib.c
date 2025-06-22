4e527bcb9d25a45184491e89d0b01ff1fedd3ab3
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-06-22 18:17:28 +0200

    chore: comment

--- lib.c
+++ lib.c
@@ -1261,6 +1261,8 @@ pg_arena_allocator_as_allocator(PgArenaAllocator *allocator) {
 pg_string_make(u64 len, PgAllocator *allocator) {
   PgString res = {0};
   res.len = len;
+  // TODO: Consider always allocating one more byte to make the conversion to c
+  // string non-allocating.
   res.data = pg_alloc(allocator, sizeof(u8), _Alignof(u8), len);
   PG_ASSERT(res.data);
   return res;
